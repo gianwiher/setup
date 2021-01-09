09eb97d76b662919cce9c5e4ec154b1de901dbd1
Gian Wiher <wiherg@student.ethz.ch>
2021-01-10 00:49:29 +0100

    initial commit

---
+++ brew.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Install Brew Packages
+brew install tree
+
+# Install MacOS Applications
+brew cask install google-chrome
+brew cask install sublime-text
+brew cask install virtualbox
+brew cask install spotify
+brew cask install discord
+brew cask install vlc
+
+# Install Source Code Pro Font
+brew tap homebrew/cask-fonts
+brew cask install font-source-code-pro
---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+############################
+
+# Run the Homebrew Script
+./brew.sh
+
+# Run the Sublime Script
+./sublime.sh
